251003c90ef82f1abae5d10bf0320c49566ceee1
Fernando Paiva <fernandopaiva1705@gmail.com>
2022-07-20 13:41:13 -0300

    Adicionado validação

--- gambit/gambit/MainWindow.xaml.cs
+++ gambit/gambit/MainWindow.xaml.cs
@@ -37,39 +37,65 @@ namespace gambit
             {
                 return;
             }
-
-
-            string descricao;
-            if (rdbEtiqueta_Adesiva.IsChecked.Value)
-                descricao = "ADESIVA";
-            else
-                descricao = "CARTAO";
-            //using (StreamWriter streamWriter = new StreamWriter($"Modelo{txtNomeLoja.Text}.txt"))
-            using FileStream fileStream = new FileStream($"MODELO-{txtarquivo.Text}.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            using StreamWriter streamWriter = new StreamWriter(fileStream);
-
-            streamWriter.WriteLine($"INSERT modeloetiqueta(CODIGO, MODELO, DESCRICAO, CUSTOMIZADA, ALTURA, LARGURA, DENSIDADE, VELOCIDADE, ETIQUETA, COLUNAS, GAP, TIPOETIQUETA) VALUES({txtCodigo.Text}, {txtModelo.Text}, N'ETIQUETA {descricao} - DIMENSOES {txtAltura.Text}x{txtLargura.Text}x{txtqtdeColunas.Text} - PADRAO', 1, {int.Parse(txtAltura.Text) * 8}, {int.Parse(txtLargura.Text) * 8 * int.Parse(txtqtdeColunas.Text)}, 10, 3, N'[CONFIGURACAO]");
-            streamWriter.WriteLine("T30\nT16\nT16\nLPPLB\n[CABECALHO]\nN\nJB\nN\nO\nZB\nq<LARGURA>\nQ<ALTURA>\n<GAP>\nS<VELOCIDADE>\nD<DENSIDADE>\nN");
-            for (int i = 0; i < int.Parse(txtqtdeColunas.Text); i++)
-            {
-                streamWriter.WriteLine($"[COLUNA{i + 1}]");
-
-                foreach (var item in lsttag.Items)
-                    streamWriter.WriteLine($"AAjusteX(0),AjusteY(0),0,1,1,2,N,\"<{item.ToString()?.ToUpper()}>\"");
-            }
-
-            var teste = fileStream.Seek(fileStream.Position, SeekOrigin.Begin);
-
-            ////fileStream.w
-            //streamWriter.WriteLine($"', 2, {gap.Text}, 1");
+                            
 
             lblStatus.Content = null;
-            lblStatus.Content = "Salvo com sucesso!";
+            if (SalvarArquivo())
+                lblStatus.Content = "Salvo com sucesso!";
+            else
+                lblStatus.Content = "Erro ao salvar!";
+        }
+
+        private bool SalvarArquivo()
+        {
+            string descricao;
+            if (rdbEtiqueta_Adesiva.IsChecked.HasValue)
+                descricao = "ADESIVA";
+            else if (rdbEtiqueta_Cartao.IsChecked.HasValue)
+                descricao = "CARTAO";
+            else
+                return false;
+
+            try
+            {
+                using FileStream fileStream = new FileStream($"MODELO-{txtarquivo.Text}-{txtModelo.Text}.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                using StreamWriter streamWriter = new StreamWriter(fileStream);
+
+                streamWriter.WriteLine($"INSERT modeloetiqueta(CODIGO, MODELO, DESCRICAO, CUSTOMIZADA, ALTURA, LARGURA, DENSIDADE, VELOCIDADE, ETIQUETA, COLUNAS, GAP, TIPOETIQUETA) VALUES({txtCodigo.Text}, {txtModelo.Text}, N'ETIQUETA {descricao} - DIMENSOES {txtLargura.Text}x{txtAltura.Text}x{txtqtdeColunas.Text} - PADRAO', 1, {int.Parse(txtAltura.Text) * 8}, {int.Parse(txtLargura.Text) * 8 * int.Parse(txtqtdeColunas.Text)}, 10, 3, N'[CONFIGURACAO]");
+                streamWriter.WriteLine("T30\nT16\nT16\nLPPLB\n[CABECALHO]\nN\nJB\nN\nO\nZB\nq<LARGURA>\nQ<ALTURA>\n<GAP>\nS<VELOCIDADE>\nD<DENSIDADE>\nN");
+                for (int i = 0; i < int.Parse(txtqtdeColunas.Text); i++)
+                {
+                    streamWriter.WriteLine($"[COLUNA{i + 1}]");
+
+                    foreach (var item in lsttag.Items)
+                    {
+
+                        if (item.ToString()?.ToUpper() == "BARRA")
+                            streamWriter.WriteLine($"BAjusteX(0),AjusteY(0),0,E30,2,2,64,B,\"<{item.ToString()?.ToUpper()}>\"");
+                        else
+                            streamWriter.WriteLine($"AAjusteX(0),AjusteY(0),0,1,1,2,N,\"<{item.ToString()?.ToUpper()}>\"");
+                    }
+                }
+
+                var teste = fileStream.Seek(fileStream.Position, SeekOrigin.Begin);
+
+                streamWriter.Flush();
+                fileStream.Seek(-2, SeekOrigin.Current);
+                streamWriter.WriteLine($"', {txtqtdeColunas.Text}, {txtgap.Text}, 1)");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+
+            return true;
+
         }
 
         private bool ValidarCampos()
         {
-            if (string.IsNullOrEmpty(txtCodigo.Text) || string.IsNullOrEmpty(txtModelo.Text) != null || string.IsNullOrEmpty(txtAltura.Text)  || string.IsNullOrEmpty(txtLargura.Text) || string.IsNullOrEmpty(txtqtdeColunas.Text))
+            if (string.IsNullOrEmpty(txtCodigo.Text) || string.IsNullOrEmpty(txtModelo.Text) || string.IsNullOrEmpty(txtAltura.Text) || string.IsNullOrEmpty(txtLargura.Text) || string.IsNullOrEmpty(txtqtdeColunas.Text) || string.IsNullOrEmpty(txtgap.Text))
             {
                 lblStatus.Content = null;
                 lblStatus.Content = "Campo não pode ser vazio";
